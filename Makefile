6f4b0fe027bfd4da4011ec415e9ffda8a57205e0
whikwon <whikwon@gmail.com>
2024-03-03 11:32:24 +0000

    Final structure

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 format:
-	black medseg/ tools/
-	isort medseg/ tools/
+	black medseg/ tools/ configs/
+	isort medseg/ tools/ configs/
--- medseg/core/trainer.py
+++ medseg/core/trainer.py
@@ -1,18 +1,22 @@
+import logging
+from abc import ABC, abstractmethod
+
 import mlflow
-from ignite.contrib.handlers import ProgressBar
 from ignite.engine import Events
 
 
-class MedSegSupervisedTrainer:
+# test는 분리해도 될 듯
+class BaseSupervisedTrainer(ABC):
     def __init__(
         self,
-        run_name,
-        cfg_path,
-        model,
-        trainer,
-        evaluator,
-        train_dataloader,
-        val_dataloader,
+        run_name=None,
+        cfg_path=None,
+        model=None,
+        trainer=None,
+        evaluator=None,
+        train_dataloader=None,
+        val_dataloader=None,
+        logger=None,
     ):
         self.run_name = run_name
         self.cfg_path = cfg_path
@@ -23,7 +27,7 @@ class MedSegSupervisedTrainer:
         self.val_dataloader = val_dataloader
         self.best_metric = -1
 
-        self.trainer.add_event_handler(Events.STARTED, self.on_started)
+        self.trainer.add_event_handler(Events.STARTED, self.on_train_started)
         self.trainer.add_event_handler(Events.EPOCH_STARTED, self.on_epoch_started)
         self.trainer.add_event_handler(Events.EPOCH_COMPLETED, self.on_epoch_completed)
         self.evaluator.add_event_handler(
@@ -35,19 +39,46 @@ class MedSegSupervisedTrainer:
         self.trainer.add_event_handler(
             Events.ITERATION_COMPLETED, self.on_iteration_completed
         )
-        self.trainer.add_event_handler(Events.COMPLETED, self.on_completed)
+        self.trainer.add_event_handler(Events.COMPLETED, self.on_train_completed)
+        if logger is None:
+            logging.basicConfig(level=logging.INFO)
+            logger = logging.getLogger()
+        self.logger = logger
 
-    def on_started(self, engine):
-        self.pbar = ProgressBar(persist=True)
-        self.pbar.attach(self.trainer, metric_names="all")
+    def on_train_started(self, engine):
         mlflow.start_run(run_name=self.run_name)
         mlflow.log_artifact(self.cfg_path)
 
+    @abstractmethod
     def on_epoch_started(self, engine):
         pass
 
     def on_epoch_completed(self, engine):
         self.evaluator.run(self.val_dataloader)
+
+    @abstractmethod
+    def on_validation_completed(self, engine):
+        pass
+
+    @abstractmethod
+    def on_iteration_started(self, engine):
+        pass
+
+    @abstractmethod
+    def on_iteration_completed(self, engine):
+        pass
+
+    def on_train_completed(self, engine):
+        mlflow.end_run()
+
+    def run(self, max_epochs):
+        self.trainer.run(self.train_dataloader, max_epochs=max_epochs)
+
+
+class ImageCasSupervisedTrainer(BaseSupervisedTrainer):
+
+    def on_epoch_started(self, engine):
+        pass
 
     def on_validation_completed(self, engine):
         metrics = self.evaluator.state.metrics
@@ -56,12 +87,10 @@ class MedSegSupervisedTrainer:
         if mean_dice > self.best_metric:
             self.best_metric = mean_dice
             mlflow.pytorch.log_model(self.model, "model")
-            self.pbar.log_message("saved new best metric model")
-        self.pbar.log_message(
-            f"Validation Results - Epoch: {engine.state.epoch} Avg accuracy: {mean_dice}"
+            self.logger.info("saved new best metric model")
+        self.logger.info(
+            f"Validation results - Epoch: {engine.state.epoch} Avg accuracy: {mean_dice}"
         )
-        self.pbar.n = self.pbar.last_print_n = 0
-        pass
 
     def on_iteration_started(self, engine):
         pass
@@ -70,8 +99,24 @@ class MedSegSupervisedTrainer:
         loss = engine.state.output
         mlflow.log_metric("train_iter_loss", loss, step=engine.state.iteration)
 
-    def on_completed(self, engine):
-        mlflow.end_run()
 
-    def run(self, max_epochs):
-        self.trainer.run(self.train_dataloader, max_epochs=max_epochs)
+class BaseSupervisedTester:
+
+    def __init__(self, dataloader, evaluator):
+        self.dataloader = dataloader
+        self.evaluator = evaluator
+
+    @abstractmethod
+    def on_test_iteration_completed(self, engine):
+        pass
+
+    @abstractmethod
+    def on_test_completed(self, engine):
+        pass
+
+    def test(self):
+        self.evaluator.run(self.dataloader)
+
+
+class ImageCasSupervisedTester(BaseSupervisedTester):
+    pass
